dac781e8121c17dfacd9c9363e5a5adc7c9b83ce
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-19 16:39:38 +0100

    js

--- javascript/basic/Image.js
+++ javascript/basic/Image.js
@@ -26,3 +26,25 @@ class Image {
     return new Image(this.url, this.size);
   }
 }
+class Image {
+  constructor(url, size) {
+    this.url = url;
+    this.size = new Size(size.width, size.height);
+  }
+  getUrl() {
+    return this.url;
+  }
+  setUrl(url) {
+    this.url = url;
+  }
+  setSize(width, height) {
+    this.size = new Size(width, height);
+  }
+  getSize() {
+    return this.size;
+  }
+  cloneImage() {
+    return new Image(this.url, this.size);
+  }
+}
+
