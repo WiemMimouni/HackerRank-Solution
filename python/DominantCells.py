8c295ef96dea9f2b061982bdc8bc2264a4c59c02
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-20 08:53:55 +0100

    python

--- python/DominantCells.py
+++ python/DominantCells.py
@@ -21,24 +21,4 @@ def numCells(grid):
     return count
 
 
-    def numCells(grid):
-    n = len(grid)
-    m = len(grid[0])
     
-    def is_dominant(i, j):
-        value = grid[i][j]
-        neighbors = [(i-1, j-1), (i-1, j), (i-1, j+1),
-                     (i, j-1),             (i, j+1),
-                     (i+1, j-1), (i+1, j), (i+1, j+1)]
-        for x, y in neighbors:
-            if 0 <= x < n and 0 <= y < m and grid[x][y] >= value:
-                return False
-        return True
-    
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if is_dominant(i, j):
-                count += 1
-    
-    return count
