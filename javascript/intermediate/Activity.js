cb9b324822d25b2722395f8414df6679bab32bfd
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-10-06 13:25:52 +0100

    edit

--- javascript/intermediate/Activity.js
+++ javascript/intermediate/Activity.js
@@ -46,3 +46,915 @@ Refund.prototype.getSender = function () {
   return this.sender;
 };
 
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
+function Activity(amount) {
+  this.amount = amount;
+}
+
+Activity.prototype.setAmount = function (value) {
+  if (value <= 0) {
+    return false;
+  }
+  this.amount = value;
+  return true;
+};
+
+Activity.prototype.getAmount = function () {
+  return this.amount;
+};
+
+function Payment(amount, receiver) {
+  Activity.call(this, amount);
+  this.receiver = receiver;
+}
+
+Payment.prototype = Object.create(Activity.prototype);
+Payment.prototype.constructor = Payment;
+
+Payment.prototype.setReceiver = function (receiver) {
+  this.receiver = receiver;
+};
+
+Payment.prototype.getReceiver = function () {
+  return this.receiver;
+};
+
+function Refund(amount, sender) {
+  Activity.call(this, amount);
+  this.sender = sender;
+}
+
+Refund.prototype = Object.create(Activity.prototype);
+Refund.prototype.constructor = Refund;
+
+Refund.prototype.setSender = function (sender) {
+  this.sender = sender;
+};
+
+Refund.prototype.getSender = function () {
+  return this.sender;
+};
+
